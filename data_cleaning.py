240fd4f289af3a2f51d324f90a4e56394242dbe7
emilielr <43408175+emilielr@users.noreply.github.com>
2022-10-11 11:00:15 +0200

    Clean up code

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 Removes rows with Skadeårsak = ukjent, rødrev, sykdom, ikke rovvilt, hund, ulykke. 
 """
@@ -6,3 +8,12 @@ def remove_specific_skadearsak(data):
     for word in words:
         data.drop(data[(data['Skadeårsak'] == word)].index, inplace=True)
     return data
+
+
+"""
+Might use later. Remove rows with empty or invalid position.
+"""
+def remove_rows_with_empty_position(data):
+    for x in data.index: 
+        if np.isnan(data.loc[x, "Lengdegrad"]) or np.isnan(data.loc[x, "Breddegrad"]):
+            data.drop(x, inplace=True)
