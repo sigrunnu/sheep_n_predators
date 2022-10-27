0a0144f704629266da9baa2b774226d7304eff2e
sigrunnu <sigrun.nummedal@gmail.com>
2022-10-27 12:55:11 +0200

    Fix.

--- features/altitude.py
+++ features/altitude.py
@@ -1,50 +1,38 @@
-import pandas as pd
-import numpy as np
-import plotly as pl
 import requests
 import math
-import os
-
-os.makedirs('cleanedData', exist_ok=True)
-print('lol')
-
-data = pd.read_csv('data/2012_koksvik_6051_2224.csv', sep='\t+', skiprows=[
-                   1, 2], engine='python', index_col=False, usecols=range(1, 18))
 
 
-counter = 1
-old_range_count = 0
-range_count = 50
-data_len = len(data)
-altitude = []
+def add_altitude(data):
 
-while counter <= math.ceil(data_len/50):
-    coordinates = []
-    for i in range(old_range_count, range_count):
-        x = data['Long'][i]
-        y = data['Lat'][i]
-        d = [x, y]
+    counter = 1
+    old_range_count = 0
+    range_count = 50
+    data_len = len(data)
+    altitude = []
 
-        coordinates.append(d)
+    while counter <= math.ceil(data_len/50):
+        coordinates = []
+        for i in range(old_range_count, range_count):
+            x = data['Long'][i]
+            y = data['Lat'][i]
+            d = [x, y]
 
-    req = requests.get(
-        f'https://ws.geonorge.no/hoydedata/v1/punkt?koordsys=4326&geojson=false&punkter={coordinates}')
+            coordinates.append(d)
 
-    for value in req.json()['punkter']:
-        altitude.append(value['z'])
+        req = requests.get(
+            f'https://ws.geonorge.no/hoydedata/v1/punkt?koordsys=4326&geojson=false&punkter={coordinates}')
 
-    old_range_count = range_count
-    range_count += 50
-    counter += 1
+        for value in req.json()['punkter']:
+            altitude.append(value['z'])
 
-    if (range_count >= data_len-1):
-        range_count -= 50
-        range_count += (data_len - range_count)
-    print(counter)
+        old_range_count = range_count
+        range_count += 50
+        counter += 1
 
+        if (range_count >= data_len-1):
+            range_count -= 50
+            range_count += (data_len - range_count)
 
-print(len(altitude))
-data['altitude'] = altitude
-print(data.head(20))
-data['Date'] = pd.to_datetime(data['Date'], yearfirst=True)
-data.to_csv('cleanedData/2012_koksvik_6051_2224_cleaned.csv')
+    data['altitude'] = altitude
+
+    return data
