90532392f6b770fb1614b8c777d6fbb019f88e8d
emilielr <43408175+emilielr@users.noreply.github.com>
2022-11-21 12:57:49 +0100

    Remove skadedato uncertain or not one date

---
+++ RemoveSkadedatoUncertainOrSeveralDates.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# data = pd.read_csv('data/rovviltskader_vasket.csv')
+
+# Remove where skadedato is usikker
+def remove_skadedato_uncertain(data):
+    return data.loc[data["Usikker skadedato"] == "Nei"]
+
+# Remove skadedato where skadedato, til and skadedato, fra is not the same 
+def remove_skadedato_not_one_date(data):
+    for x in data.index:
+        skadedato_from = data.loc[x, "Skadedato, fra"]
+        skadedato_to = data.loc[x, "Skadedato, til"]
+        
+        if skadedato_from != skadedato_to:
+            data.drop(x, inplace=True)
+    
+    return data
+
+def remove_skadedato_uncertain_and_not_one_date(data):
+    data1 = remove_skadedato_uncertain(data)
+    data2 = remove_skadedato_not_one_date(data1)
+    return data2
+
