6efabfc8e1dbd1a9d4dae7eba41bd0985a3b9695
emilielr <43408175+emilielr@users.noreply.github.com>
2022-11-30 15:05:58 +0100

    remove source-ids with less than 10 occurances

--- cleaning/Kaasa.py
+++ cleaning/Kaasa.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from RemoveDuplicates import remove_duplicates
-from CleanKaasa import clean_individual_nr_not_null, change_dtypes, match_source_id_to_individual
-
+from CleanKaasa import match_source_id_to_individual, remove_sheep_with_less_than_10_points
+from FormatKaasa import replace_individual_nr_with_null_values, change_dtypes
 
 def iterate_over_all_files():
     files = ['kaasa_2021.csv', 'kaasa_2020.csv', 'kaasa_2019.csv',
@@ -12,14 +12,15 @@ def iterate_over_all_files():
         data = pd.read_csv(filepath)
 
         if not data.empty:
-            new = remove_duplicates(data) #DONE
-            new2 = clean_individual_nr_not_null(new) #DONE
-            new3 = change_dtypes(new2) #DONE 
-            new4 = match_source_id_to_individual(new3) #DONE
-            new5 = new4.sort_values(by=['individual', 'date_time']) #DONE
+            #new = remove_duplicates(data) #DONE
+            #new2 = replace_individual_nr_with_null_values(new) #DONE
+            #new3 = change_dtypes(new2) #DONE 
+            #new4 = match_source_id_to_individual(new3) #DONE
+            #new5 = remove_sheep_with_less_than_10_points(data) #DONE
+            new6 = data.sort_values(by=['individual', 'date_time']) #DONE
 
-            if not new5.empty:
-                new5.to_csv(filepath, index=False)
+            if not new6.empty:
+                new6.to_csv(filepath, index=False)
                 print('Lagret til fil:', file)
 
 
