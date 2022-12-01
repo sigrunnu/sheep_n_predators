24da417d4c75ee49a98d4f9fbb3ea45a95286f5c
sigrunnu <sigrun.nummedal@gmail.com>
2022-12-01 13:26:41 +0100

    Temperature for 4 hour interval position.

--- features/Velocity.py
+++ features/Velocity.py
@@ -1,12 +1,14 @@
 from Distance import calculate_distance
 
 """
-Calculate the velocit of the sheep based on the Haversine formula in m/h.
+Calculate the velocity of the sheep based on the Haversine formula in m/h.
 """
+
 
 def calculate_velocity(lat1, long1, lat2, long2):
     dist = calculate_distance(lat1, long1, lat2, long2)
     return round(dist/0.5, 3)
+
 
 def add_velocity(data):
     data['velocity'] = 0
@@ -18,7 +20,7 @@ def add_velocity(data):
         lat2 = data.loc[i+1, 'Lat']
 
         velocity = calculate_velocity(lat1, long1, lat2, long2)
-        
+
         data.loc[i+1, 'velocity'] = velocity
-    
+
     return data
