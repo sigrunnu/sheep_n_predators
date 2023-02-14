04509e19185b7055bcb42817c7c477687fe2004a
sigrunnu <sigrun.nummedal@gmail.com>
2023-02-14 15:04:37 +0100

    Remove outliers.

--- cleaning/RemoveOutliers.py
+++ cleaning/RemoveOutliers.py
@@ -2,67 +2,94 @@ import pandas as pd
 from haversine import haversine, Unit
 
 
-def loop_individual(data):
+# First loop trough all individuals to make sure the distance between each individual will not alter the result
+def remove_outliers(data):
     individuals = data.individual.unique()
     for i in individuals:
         individual = data[data['individual'] == i]
-        find_movement_window(individual)
+        start_index = individual.first_valid_index()
+        end_index = start_index + len(individual)
+        find_movement_window(start_index, end_index, data)
+    return data
 
 
-def find_movement_window(individual):
-    for x in individual.index:
-        if (x <= 3):
-            group = individual.iloc[0: 5]
-        elif (x >= len(individual) - 2):
-            group = individual.iloc[len(individual) - 5: len(individual)]
-
+def find_movement_window(start_index, end_index,  data):
+    # Pick out the current row plus the 4 adjacent and find a movement window of five rows
+    for x in range(start_index, end_index):
+        if (x < start_index + 3):
+            group = data.iloc[start_index: start_index + 5]
+        elif (x >= end_index - 2):
+            group = data.iloc[end_index - 5: end_index]
         else:
-            group = individual.iloc[x-2: x+3]
+            group = data.iloc[x-2: x+3]
 
-        lat = group['st_y']
-        long = group['st_x']
+        lat = group['latitude']
+        long = group['longitude']
 
-        remove_outliers_median(
-            individual, x, lat.median(), long.median(), 100000)
-        #remove_outliers_mean(individual, x, lat.mean(), long.mean(), 50000)
+        # Find out the mean of the time between each row
+        date_time = pd.to_datetime(group['date_time']).reset_index(drop=True)
+        time_diff = []
+        for i in range(len(date_time)-1):
+            diff = date_time[i]-date_time[i+1]
+            time_diff.append(abs(diff.total_seconds()/3600))
+        # mean_hours are used to find what the distance limit from x to the mean should be
+        mean_hours = sum(time_diff)/5
+
+        # This one should be run before remove_outliers_mean
+        remove_outliers_median(x, lat.median(), long.median(), 100000, data)
+
+        remove_outliers_mean(x, lat.mean(), long.mean(), data, mean_hours)
 
 
-def remove_outliers_median(individual, x, lat_median, long_median, value):
+# Find and replace the outliers with a spesific distance to the median of the movement window
+
+
+def remove_outliers_median(x, lat_median, long_median, distance_limit, data):
     distance_median = distance_to_x(
-        individual.loc[x, 'st_y'], individual.loc[x, 'st_x'], lat_median, long_median)
-    if (distance_median > value):
-        print(distance_median)
-        print(x)
-        print(data.loc[x])
-        new_coordinates = generate_new_coordinates(x)
-        print(new_coordinates)
+        data.loc[x, 'latitude'], data.loc[x, 'longitude'], lat_median, long_median)
+    if (distance_median > distance_limit):
+        print("The distance from x to the median is", distance_median)
+        print("Date of current row", data.loc[x, 'date_time'])
+        print("Individual of current row", data.loc[x, 'individual'])
+        print('-------------------')
+        generate_new_coordinates(x, data)
 
 
-def remove_outliers_mean(individual, x, lat_mean, long_mean, value):
+def remove_outliers_mean(x, lat_mean, long_mean, data,  mean_hours):
+    # Find and replace the outliers with a spesific distance to the mean of the movement window
+
+    # find distance from x to the mean distance
     distance_mean = distance_to_x(
-        individual.loc[x, 'st_y'], individual.loc[x, 'st_x'], lat_mean, long_mean)
-    if (distance_mean > value):
-        print(distance_mean)
-        print(x)
-        new_coordinates = generate_new_coordinates(x)
-        print(generate_new_coordinates(x))
-        data.loc[x, "st_y"] = new_coordinates[0]
-        data.loc[x, "st_x"] = new_coordinates[1]
-        print(data.loc[x])
+        data.loc[x, 'latitude'], data.loc[x, 'longitude'], lat_mean, long_mean)
+
+    # Set the lowest limit to 15km/h * 1
+    mean_hours = mean_hours if mean_hours > 1 else 1
+    # Set the lowest limit to 15km/h * 1
+    mean_hours = 6 if mean_hours > 5 else mean_hours
+
+    # The distance is found by multiplying 15 km/h with the number of hours between each point because a sheep can not run 15km/hour
+    distance_limit = 15000 * mean_hours
+
+    # If the distance is more than the limit set, then new coordinates are made for the data row
+    if (distance_mean > distance_limit):
+        print("The distance from x to the mean is", distance_mean)
+        print("Date of current row", data.loc[x, 'date_time'])
+        print("Individual of current row", data.loc[x, 'individual'])
+        print('-------------------')
+        generate_new_coordinates(x, data)
 
 
-def generate_new_coordinates(x):
-    lat = (data.loc[x-1, 'st_y'] + data.loc[x-1, 'st_y'])/2
-    long = (data.loc[x-1, 'st_x'] + data.loc[x-1, 'st_x'])/2
-    return [lat, long]
+def generate_new_coordinates(x, data):
+    # Generates new coordinates based on the mean of the two adjacent rows
+    lat = (data.loc[x-1, 'latitude'] +
+           data.loc[x+1, 'latitude'])/2
+    long = (data.loc[x-1, 'longitude'] +
+            data.loc[x + 1, 'longitude'])/2
+    data.loc[x, "latitude"] = lat
+    data.loc[x, "longitude"] = long
 
 
 def distance_to_x(x_lat, x_long, lat_median, long_median):
     t1 = (x_lat, x_long)
     t2 = (lat_median, long_median)
     return haversine(t1, t2, unit=Unit.METERS)
-
-
-data = pd.read_csv('data/kaasa/kaasa_2021.csv')
-print(loop_individual(data))
-data.to_csv('data/kaasa/test.csv', index=False)
