3417a5b862794f395f143d4252a16dd723e52f08
sigrunnu <sigrun.nummedal@gmail.com>
2023-02-16 12:58:36 +0100

    New temperatures and fixed temperature script.

--- features/TemperatureEveryHour.py
+++ features/TemperatureEveryHour.py
@@ -12,11 +12,11 @@ def add_temperature(sheep_data, weather_data):
         ",", ".")
 
     sheep_data['temperature'] = ''
-
     for i in range(len(sheep_data)):
 
         date = pd.to_datetime(sheep_data.loc[i, 'date_time'])
-        date = date.replace(hour=0, minute=0, second=0)
+        new_hour = date.hour + 1 if date.minute > 30 and date.hour != 23 else date.hour
+        date = date.replace(hour=new_hour, minute=0, second=0)
 
         row = weather_data.loc[weather_data['Tid(norsk normaltid)']
                                == date].index
@@ -25,6 +25,14 @@ def add_temperature(sheep_data, weather_data):
             temp = weather_data.loc[row[0], 'Lufttemperatur']
             sheep_data.at[i, 'temperature'] = temp
         else:
+            date = sheep_data.at[i, 'date_time']
+            print("Date does not exist", date)
             # If the date do not exist in the weather data
             sheep_data.at[i, 'temperature'] = None
     return sheep_data
+
+
+weather = pd.read_csv('../data/weather/kaasa_2021.csv', sep=";")
+data = pd.read_csv('../data/kaasa/kaasa_2021.csv', sep=",")
+
+add_temperature(data, weather)
