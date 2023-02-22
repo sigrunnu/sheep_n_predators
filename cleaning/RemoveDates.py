55dd42c3029d4ba5ad6998f9b2788dde97f480c5
emilielr <43408175+emilielr@users.noreply.github.com>
2023-02-22 11:30:48 +0100

    Add comments to code

--- cleaning/RemoveDates.py
+++ cleaning/RemoveDates.py
@@ -37,7 +37,7 @@ def remove_dates_where_sheep_not_on_pastures(data, filename, start, end):
 
 
 """
-Remove all data from oct - june.
+Remove all data from oct - june. Considered not to on outfield pastures.
 """
 def remove_rows_from_oct_to_jun(data):
     data['date_time'] = pd.to_datetime(data['date_time'], yearfirst=True)
@@ -51,8 +51,12 @@ def remove_rows_from_oct_to_jun(data):
     return filter
 
 
-
-# Get date values for individual
+"""
+Get date values for individual
+dates_unique: list of unique dates
+first_date: the first date of the sheep
+end_date: the last date of the sheep
+"""
 def get_date_values(i_data):
     dates = pd.to_datetime(i_data['date_time']).dt.date # get only date and not time stamp
     dates_unique = dates.unique()
@@ -61,18 +65,21 @@ def get_date_values(i_data):
     return dates_unique, first_date, end_date
 
 
-# Return list with missing dates
+"""
+Return list with missing dates between start and end date
+"""
 def missing_dates(dates_unique, first_date, end_date):
     return pd.date_range(start=first_date, end=end_date).difference(dates_unique)
 
 
 """
-Check if each sheep have any missing date-values, and if they have over 10 missing values - delete the sheep.
+Delete sheep if they have over 10 missing date-values.
+Return new dataframe with no sheep that has more than 10 missing values. 
 """
 def delete_sheep_with_missing_dates(df):
     individuals = df.individual.unique() # list of individuals
     count_individual_w_missing_dates = 0
-    occur_missing_dates = dict()
+    occur_missing_dates = dict() # only to help et an overview over sheep and their number of missing dates
     
     for ind in individuals:
         i_data = df[df['individual'] == ind]
@@ -80,8 +87,8 @@ def delete_sheep_with_missing_dates(df):
         missing_dates1 = missing_dates(dates_unique1, first_date1, end_date1) # return list with missing dates
         
         if len(missing_dates1) > 0:
-            print('For individ', ind, 'mangler følgende datoer: ', missing_dates1, '. Antall: ', len(missing_dates1))
-            print('-------------------------------------------------------------------------------------------------')
+            #print('For individ', ind, 'mangler følgende datoer: ', missing_dates1, '. Antall: ', len(missing_dates1))
+            #print('-------------------------------------------------------------------------------------------------')
             count_individual_w_missing_dates += 1
             occur_missing_dates[ind] = len(missing_dates1)
 
@@ -89,6 +96,7 @@ def delete_sheep_with_missing_dates(df):
 
     for x in occur_missing_dates:
         print('Individ: ', x, 'mangler ', occur_missing_dates[x], 'datoer')
+        
         # If sheep has more than ten missing days, delete the sheep
         if occur_missing_dates[x] >= 10:
             ind_index = df[df['individual'] == x].index
@@ -97,7 +105,10 @@ def delete_sheep_with_missing_dates(df):
     
     return df
 
-
+"""
+Function to delete sheep if they have less than 15 unique dates. 
+Return new dataframe with sheep that only has over 15 unique dates.
+"""
 def delete_sheep_if_less_than_15_dates(df):
     individuals = df.individual.unique() # list of individuals
 
